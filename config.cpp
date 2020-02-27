9f9ec367531528f869c337d31c2bc5d57528fce7
Stian Onarheim <stian.onarheim@gmail.com>
2020-02-27 20:40:42 +0100

    Removed cached files

--- config.cpp
+++
@@ -1,6 +0,0 @@
-#include <QString>
-
-const QString dbHostName = "feqzz.no";
-const QString dbDatabaseName = "qt_tracker";
-const QString dbUserName = "qtada";
-const QString dbPassword = "nj.BYb@3*rK@LNAubjqK7px7NNqPc3XLJpGT!XPXazb4A@fDXz";
