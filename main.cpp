d56d5d917200f41acd965114b37029a4d8a2823c
tarald <taraldv@gmail.com>
2020-02-29 22:38:14 +0100

    glemte main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,21 @@
 #include <QQmlEngine>
 #include <QQmlContext>
 #include "model/login.h"
-<<<<<<< HEAD
 #include "core/securesocket.h"
-=======
 #include "model/invite.h"
 #include "model/user.h"
->>>>>>> test
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
-<<<<<<< HEAD
-    QScopedPointer<Login> login(new Login);
     QScopedPointer<SecureSocket> secureSocket(new SecureSocket);
-=======
     User sessionUser;
-
     QScopedPointer<Login> login(new Login(&sessionUser));
     QScopedPointer<Invite> invite(new Invite);
     QScopedPointer<User> user(&sessionUser);
->>>>>>> test
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
@@ -34,12 +27,9 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.rootContext()->setContextProperty("login", login.data());
-<<<<<<< HEAD
     engine.rootContext()->setContextProperty("secureSocket", secureSocket.data());
-=======
     engine.rootContext()->setContextProperty("invite", invite.data());
     engine.rootContext()->setContextProperty("user", user.data());
->>>>>>> test
     engine.load(url);
 
     //
