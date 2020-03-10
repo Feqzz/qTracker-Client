440210834b93d8d5c7187513e01359bc90ddc9a2
Stian Onarheim <stian.onarheim@gmail.com>
2020-03-10 20:05:08 +0100

    Modified the db for torrents

--- makeTables.sql
+++ makeTables.sql
@@ -1,6 +1,9 @@
-
 DROP TABLE IF EXISTS text;
 DROP TABLE IF EXISTS invite;
+DROP TABLE IF EXISTS scrapeLog;
+DROP TABLE IF EXISTS filesUsers;
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS announceLog;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE IF NOT EXISTS user
@@ -9,11 +12,15 @@ CREATE TABLE IF NOT EXISTS user
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(191) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    upload DOUBLE,
-    download DOUBLE,
-    points INT,
+    upload DOUBLE UNSIGNED NOT NULL DEFAULT 0,
+    download DOUBLE UNSIGNED NOT NULL DEFAULT 0,
+    points INT UNSIGNED,
     privilege INT,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    canLeech TINYINT NOT NULL DEFAULT 1,
+    peersLimit INT NOT NULL DEFAULT 0,
+    torrentsLimit INT NOT NULL DEFAULT 0,
+    torrentPass CHAR(32) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS invite
@@ -35,6 +42,73 @@ CREATE TABLE IF NOT EXISTS text
     lastEditedAt DATETIME NOT NULL,
     lastEditedBy INT NOT NULL,
     FOREIGN KEY (lastEditedBy) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS scrapeLog
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ipa INT UNSIGNED NOT NULL,
+    infoHash BINARY(20),
+    userId INT NOT NULL,
+    modifiedTime DATETIME NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS files
+(
+    fileId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    infoHash BINARY(20) NOT NULL,
+    leechers INT NOT NULL DEFAULT 0,
+    seeders INT NOT NULL DEFAULT 0,
+    completed INT NOT NULL DEFAULT 0,
+    flags INT NOT NULL DEFAULT 0,
+    modifiedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    createdTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY (infoHash)
+);
+
+
+CREATE TABLE IF NOT EXISTS filesUsers
+(
+    fileId INT NOT NULL,
+    userId INT NOT NULL,
+    isActive TINYINT NOT NULL,
+    announced INT NOT NULL,
+    completed INT NOT NULL,
+    downloaded BIGINT UNSIGNED NOT NULL,
+    `left` BIGINT UNSIGNED NOT NULL,
+    uploaded BIGINT UNSIGNED NOT NULL,
+    modifiedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY (fileId, userId),
+    FOREIGN KEY (userId) REFERENCES user(id),
+    FOREIGN KEY (fileId) REFERENCES files(fileId)
+);
+
+CREATE TABLE IF NOT EXISTS denyFromHosts
+(
+    `begin` INT UNSIGNED NOT NULL,
+    `end` INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS denyFromClients
+(
+    peerId char(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS announceLog
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ipa INT UNSIGNED NOT NULL,
+    port INT NOT NULL,
+    event INT NOT NULL,
+    infoHash BINARY(20) NOT NULL,
+    peerId BINARY(20) NOT NULL,
+    downloaded BIGINT UNSIGNED NOT NULL,
+    `left` BIGINT UNSIGNED NOT NULL,
+    uploaded BIGINT UNSIGNED NOT NULL,
+    userId INT NOT NULL,
+    modifiedTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES user(id)
 );
 
 /* password:123 */
