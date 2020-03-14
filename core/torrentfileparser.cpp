ce162453c56c546fd3fb111ea8ebefa27984a3e5
Tarald <taraldv@gmail.com>
2020-03-14 16:45:03 +0100

    changed from QFile to ifstream

--- core/torrentfileparser.cpp
+++ core/torrentfileparser.cpp
@@ -27,47 +27,16 @@ void TorrentFileParser::parse(QString url)
     QString fileUrlSubstring = url.mid(7);
 
 
-    QFile file(fileUrlSubstring);
+    /*QFile file(fileUrlSubstring);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         qDebug() << "Failed to read file at: "<< fileUrlSubstring;
     }
+    std::istringstream is(file.readAll().toStdString());*/
 
-    /*std::istringstream is(file.readAll().toStdString());
-    auto data = bencode::decode(is, bencode::no_check_eof);
-    auto list = boost::get<bencode::dict>(data);
-    auto pos = list.find("announce");
-    if (pos == list.end()) {
-        std::cout << "Not found announce\n";
-    } else {
-        auto value = boost::get<bencode::string>(pos->second);
-        std::cout << value << "\n";
-    }
-    pos = list.find("created by");
-    if (pos == list.end()) {
-        std::cout << "Not found Created by\n";
-    } else {
-        auto value = boost::get<bencode::string>(pos->second);
-        std::cout << value << "\n";
-    }
-    pos = list.find("creation date");
-    if (pos == list.end()) {
-        std::cout << "Not found CreationDate\n";
-    } else {
-        auto value = boost::get<bencode::integer>(pos->second);
-        std::cout << value << "\n";
-    }
-    pos = list.find("info");
-    if (pos == list.end()) {
-        std::cout << "Not found info\n";
-    } else {
-        auto value = boost::get<bencode::dict>(pos->second);
-        //std::cout << value << "\n";
-    }*/
-
-
-    std::istringstream is(file.readAll().toStdString());
-    auto data = bencode::decode(is, bencode::no_check_eof);
+    std::ifstream ifs;
+    ifs.open(fileUrlSubstring.toLocal8Bit(), std::ifstream::in);
+    auto data = bencode::decode(ifs, bencode::no_check_eof);
     auto all = boost::get<bencode::dict>(data);
 
     auto pos = all.find("announce");
